2e2b2994e85dcc2b4feebde0e9f00ed4f9e3f809
Th3Coder <th3noob8389@gmail.com>
2024-01-30 19:59:31 +0530

    Delete Simple Programs directory

--- Simple Programs/Area Of Rectangle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    int l,b,area;
-    printf("Enter Length & Breadth Of Rectangle:");
-    scanf("%d %d", &l, &b);
-    area = l*b;
-    printf("Area Of Rectangle Is: %d",area);
-    return 0;
-}
