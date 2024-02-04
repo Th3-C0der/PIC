d080077109ecb08a63eec0629c6611e03fdfb08c
Th3Coder <th3noob8389@gmail.com>
2024-02-04 19:39:12 +0530

    ^_^

--- PIC-Codes/Value Swap.c
+++ PIC-Codes/Value Swap.c
@@ -1,20 +1,16 @@
-// Value Swap Code By Th3Coder
+// Value Swap Code By Th3-C0der
 #include<stdio.h>
 #include<conio.h>
 void main() {
     int a, b, c;
-    printf("Enter Value Of a:");
-    scanf("%d", &a);
-    printf("Enter Value Of b:");
-    scanf("%d", &b);
+    printf("Enter Value Of a & b:");
+    scanf("%d%d", &a, &b);
     printf("Values Before Swap\n");
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
+    printf("a = %d\nb = %d\n",a,b);
     c = a;
     a = b;
     b = c;
     printf("Values After Swap\n");
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
+    printf("a = %d\nb = %d\n",a,b);
     getch();
 }
